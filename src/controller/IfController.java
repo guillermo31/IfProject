d35512bc80f7b0b546f815229319d74b56823833
Guillermo <memo33101@gmail.com>
2018-09-20 08:38:03 -0600

    created getters and setters for Sport Object's properties

--- src/controller/IfController.java
+++ src/controller/IfController.java
@@ -1,7 +1,19 @@
 package controller;
 
+
 public class IfController
-{
+{	
+	/**
+	 * builds the instance of the controller 
+	 */
+	public IfController() 
+	{
+		
+	}
+	
+	/**
+	 * where methods are called 
+	 */
 	public void start()
 	{
 		
--- src/model/Sport.java
+++ src/model/Sport.java
@@ -4,22 +4,77 @@ public class Sport
 {
 	//data members 
 	private String name;
-	private String activity;
+	private String sport;
 	private int intensity;
 	private int timeMinutes;
 	private boolean isEnjoyable;
 	
 	//always write data members at the beginning of class
 	
-	public Sport(String name, String activity, int intensity, int timeMinutes, boolean isEnjoyable) 
+	/*
+	 * Default constructor for after the fact customization.
+	 * AKA I don't know the info yet.
+	 */
+	public Sport()
+	{
+		
+	}
+	
+	/*
+	 * Better constructor when I know information for the Sport object.
+	 * @param user's favorite sport's information.
+	 */
+	public Sport(String name, String sport, int intensity, int timeMinutes, boolean isEnjoyable) 
 	{
 		this.name = name;
-		this.activity = activity;
+		this.sport = sport;
 		this.intensity = intensity;
 		this.timeMinutes = timeMinutes;
 		this.isEnjoyable = isEnjoyable;
 	}
 	
+	//getters 
 	
+	public String getName()
+	{
+		return name;
+	}
+	public String getSport()
+	{
+		return sport;
+	}
+	public int getIntensity()
+	{
+		return intensity;
+	}
+	public int getTimeMinutes()
+	{
+		return timeMinutes;
+	}
+	public boolean getIsEnjoyable()
+	{
+		return isEnjoyable;
+	}
 	
+	//setters
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public void setSport(String sport) 
+	{
+		this.sport = sport;
+	}
+	public void setIntensity(int intensity)
+	{
+		this.intensity = intensity;
+	}
+	public void setTimeMinutes(int timeMinutes)
+	{
+		this.timeMinutes = timeMinutes;
+	}
+	public void setIsEnjoyable(boolean isEnjoyable)
+	{
+		this.isEnjoyable = isEnjoyable;
+	}
 }
