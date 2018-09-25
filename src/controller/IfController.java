a60d9dbbe31df562339a7d604146c5093e4a3f7e
Guillermo <memo33101@gmail.com>
2018-09-25 08:50:15 -0600

    added for loop, and started input loop. Added validDouble method

--- src/controller/IfController.java
+++ src/controller/IfController.java
@@ -20,8 +20,26 @@ public class IfController
 	public void start()
 	{
 		loop();
+		askUser();
 	}
 	
+	public boolean validDouble(String maybeDouble)
+	{
+		boolean isValid = false;
+		
+		
+		try
+		{
+			Double.parseDouble(maybeDouble);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "you should type a double like 0.52542");
+		}
+		
+		return isValid;
+	}
 	
 	private void loop()
 	{
@@ -37,6 +55,22 @@ public class IfController
 			{
 				isDone = true;
 			}
+		}
+		
+		for (int loopy = 0; loopy < 30; loopy +=2) 
+		{
+			JOptionPane.showMessageDialog(null, "the value is: " + loopy);
+		}
+	}
+	
+	
+	private void askUser() 
+	{
+		String response = JOptionPane.showInputDialog(null,"What is the distance?");
+		
+		while (!validDouble(response))
+		{
+			response = JOptionPane.showInputDialog(null,"no!, type in a valid number for the distance");
 		}
 	}
 	
